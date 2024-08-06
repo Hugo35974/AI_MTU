102d6adfb6d35bf79f5b95c0df711cb02691492d
Hugo Bénard <>
2024-08-06 11:47:33 +0100

    Add modification for running in macos

--- Main.py
+++ Main.py
@@ -2,36 +2,36 @@ from src.Runner.Runner import Run
 
 ml_pipeline = [
     "BayesianRidge",
-    "DummyRegressor",
-    "GaussianProcessRegressor",
-    "KernelRidge",
-    "KNeighborsRegressor",
-    "NuSVR",
-    "PassiveAggressiveRegressor",
-    "SGDRegressor",
-    "XGBRegressor",
-    "RandomForestRegressor",
-    "GradientBoostingRegressor",
-    "LinearRegression",
-    "Ridge",
-    "Lasso",
-    "ElasticNet",
-    "SVR",
-    "DecisionTreeRegressor",
-    "MLPRegressor"
+    # "DummyRegressor",
+    # "GaussianProcessRegressor",
+    # "KernelRidge",
+    # "KNeighborsRegressor",
+    # "NuSVR",
+    # "PassiveAggressiveRegressor",
+    # "SGDRegressor",
+    # "XGBRegressor",
+    # "RandomForestRegressor",
+    # "GradientBoostingRegressor",
+    # "LinearRegression",
+    # "Ridge",
+    # "Lasso",
+    # "ElasticNet",
+    # "SVR",
+    # "DecisionTreeRegressor",
+    # "MLPRegressor"
 ]
 
 runner_ml = Run(ml_pipeline, plot = True)
 runner_ml.build_pipeline()
 
-dl_functions = [
-    "LSTM_Model",
-    "GRU_Model",
-    "RNN_Model"
-    ]
+# dl_functions = [
+#     "LSTM_Model",
+#     "GRU_Model",
+#     "RNN_Model"
+#     ]
 
-runner_dl = Run(dl_functions, plot = True)
-runner_dl.build_nn_pipeline()
+# runner_dl = Run(dl_functions, plot = True)
+# runner_dl.build_nn_pipeline()
 
 # ml_run_model = [
 #     # "KNeighborsRegressor_MinMaxScaler",
